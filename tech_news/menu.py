126ea9309fdcecf2fa3065cc84868ee7a8f4d79e
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-12 18:30:15 -0300

    feat: add functions calls in menu function

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,4 +1,30 @@
 import sys
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_tag,
+    search_by_category
+)
+from tech_news.scraper import get_tech_news
+
+
+def print_top_five(option):
+    if option == "5":
+        print(top_5_news())
+    elif option == "6":
+        print(top_5_categories())
+
+
+def search_by(option, param):
+    if option == "1":
+        print(search_by_title(param))
+    elif option == "2":
+        print(search_by_date(param))
+    elif option == "3":
+        print(search_by_tag(param))
+    else:
+        print(search_by_category(param))
 
 
 # Requisito 12
@@ -15,15 +41,22 @@ def analyzer_menu():
         " 7 - Sair."
     )
 
-    if option == 0:
-        input("Digite quantas notícias serão buscadas:")
-    elif option == 1:
-        input("Digite o título:")
-    elif option == 2:
-        input("Digite a data no formato aaaa-mm-dd:")
-    elif option == 3:
-        input("Digite a tag:")
-    elif option == 4:
-        input("Digite a categoria:")
+    parameters_options = {
+        "1": "Digite o título:",
+        "2": "Digite a data no formato aaaa-mm-dd:",
+        "3": "Digite a tag:",
+        "4": "Digite a categoria:",
+    }
+
+    if option == "0":
+        news_quantity = input("Digite quantas notícias serão buscadas:")
+        print(get_tech_news(news_quantity))
+    elif option in ["1", "2", "3", "4"]:
+        parameter = input(parameters_options[option])
+        search_by(option, parameter)
+    elif option in ["5", "6"]:
+        print_top_five(option)
+    elif option == "7":
+        print("Encerrando script")
     else:
-        sys.stderr.write("Opção inválida")
+        sys.stderr.write("Opção inválida\n")
