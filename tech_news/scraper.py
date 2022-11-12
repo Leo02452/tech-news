c4fadc4c979819462fb52c78d4c172317fb11f37
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-12 12:34:32 -0300

    feat: implements scrape new detail function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,7 +36,23 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return {
+                "url": selector.css('link[rel=canonical]::attr(href)').get(),
+                "title": selector.css('h1.entry-title::text').get().strip(),
+                "timestamp": selector.css('li.meta-date::text').get(),
+                "writer": selector.css('span.author > a::text').get(),
+                "comments_count": len(
+                    selector.css('div.coment-content').getall()
+                ),
+                "summary": "".join(
+                    selector.css(
+                        "div.entry-content > p:nth-of-type(1) *::text"
+                    ).getall()
+                ).strip(),
+                "tags": selector.css('a[rel=tag]::text').getall(),
+                "category": selector.css('span.label::text').get(),
+            }
 
 
 # Requisito 5
