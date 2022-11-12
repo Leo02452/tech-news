1176d7246c399bbbd3c3302c6c932958f7cc5258
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-12 17:55:59 -0300

    feat: implements analyzer menu function

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,29 @@
+import sys
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    option = input(
+        "Selecione uma das opções a seguir:\n"
+        " 0 - Popular o banco com notícias;\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por tag;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair."
+    )
+
+    if option == 0:
+        input("Digite quantas notícias serão buscadas:")
+    elif option == 1:
+        input("Digite o título:")
+    elif option == 2:
+        input("Digite a data no formato aaaa-mm-dd:")
+    elif option == 3:
+        input("Digite a tag:")
+    elif option == 4:
+        input("Digite a categoria:")
+    else:
+        sys.stderr.write("Opção inválida")
