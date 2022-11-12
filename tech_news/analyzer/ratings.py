3306daaf1e25d45eb65ae82a912e449dd94dd65e
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-12 17:16:08 -0300

    feat: implements get top 5 news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,16 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    news_list.sort(
+        reverse=True, key=lambda new: new['comments_count']
+    )
+    return [
+            (new['title'], new['url'])
+            for new in news_list
+        ][:5]
 
 
 # Requisito 11
