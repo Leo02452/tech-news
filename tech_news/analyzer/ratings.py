b86f8e32daa5fad1482af04ec2b560c72f3c8294
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-12 17:16:22 -0300

    feat: implements get top 5 categories function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+import collections
 
 
 # Requisito 10
@@ -15,4 +16,14 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    categories_list = [new['category'] for new in news_list]
+
+    categories_occurrences = collections.Counter(categories_list)
+    new_categories_list = list(categories_occurrences.keys())
+    new_categories_list.sort()
+    new_categories_list.sort(
+        key=lambda i: categories_occurrences[i], reverse=True
+    )
+
+    return new_categories_list[:5]
